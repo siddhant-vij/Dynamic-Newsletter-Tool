56d69c5674ce7fe36457ab2c7a79766a52f525e0
Siddhant Vij <siddhantvij201@gmail.com>
2024-04-14 17:24:36 +0530

    Basic test for generating index.html
    - With styling & assets applied.
    - With gohtml templates implemented with Go-types.

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -1,1 +1,34 @@
 package common
+
+type DynamicContent struct {
+	PageTitle   string
+	PageContent PageContent
+}
+
+type PageContent struct {
+	Header             string
+	Date               string
+	Description        string
+	AchievementsTitle  string
+	Achievements       []Achievement
+	LatestUpdatesTitle string
+	LatestBlogs        []LatestBlog
+	LatestButtonTitle  string
+	SenderName         string
+	SenderOccupation   string
+	CurrentYear        string
+}
+
+type LatestBlog struct {
+	LatestBlogImage       string
+	LatestBlogTitle       string
+	LatestBlogDate        string
+	LatestBlogDescription string
+}
+
+type Achievement struct {
+	AchievementImage       string
+	AchievementTitle       string
+	AchievementDescription string
+	AchievementButtonText  string
+}
